45085c2dcd78e95f4c985532aabeb8eb2f06acab
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Counter } from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+
+      <h1>React + Vite</h1>
+      <h2>On CodeSandbox!</h2>
+    </div>
+  );
+}
+
+export default App;
